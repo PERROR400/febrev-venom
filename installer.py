75cc546034060a01444c01f012fb6f9ea2ab3831
febinrev <52229330+febinrev@users.noreply.github.com>
2019-07-20 18:34:21 +0530

    Add files via upload

--- installer.py
+++ installer.py
@@ -17,6 +17,21 @@ python3=input("IS PYTHON3 INSTALLED IN YOUR SYSTEM? [Y/n] : ")
 if python3=="n" or python3=="N":
            os.system("sudo apt-get install python3")
 
+apache=input("IS APACHE2 SERVER IS INSTALLED IN YOUR SYSTEM? [Y/n] : ")
+if apache=="n" or apache=="N":
+          os.system("sudo apt-get install apache2")
+else:
+     print("HURRAY....")
+
+path=os.getcwd()
+with open("febrev.sh","w+") as fr:
+      fr.write(f"python3 {path}/febrev-venom.py")
+os.system(f"cp {path}/febrev.sh /bin/febrev1")
+os.system("chmod +x /bin/febrev.sh")
+print("")
+print("NOW YOU CAN RUN FEBREV-VENOM FROM ANYWERE BY TYPING   >> ./febrev.sh")
+
+
 
 
 print("STARTING FEBREV VENOM......#######################")
